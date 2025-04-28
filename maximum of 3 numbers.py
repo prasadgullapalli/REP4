ffc291fd788dab600d8e9354299be7619f126ad7
prasadgullapalli <140532598+prasadgullapalli@users.noreply.github.com>
2025-04-28 14:58:46 +0530

    Update maximum of 3 numbers.py

--- maximum of 3 numbers.py
+++ maximum of 3 numbers.py
@@ -1,9 +1,10 @@
-a=int(input("enter a value"))
-b=int(input("enter b value"))
-c=int(input("enter c value"))
-if a>b and a>c:
-    print("maximum is:",a)
-elif b>c:
-    print("maximum is:",b)
+a = 10
+b = 20
+c = 15
+
+if a > b and a > c:
+    print("maximum is:", a)
+elif b > c:
+    print("maximum is:", b)
 else:
-    print("maximum is :",c)
+    print("maximum is:", c)
